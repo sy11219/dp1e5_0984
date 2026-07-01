b7bc8c1d420c177a43f4a2885d88e55a044bf6bf
sy11219 <120681844+sy11219@users.noreply.github.com>
2026-06-30 20:08:44 -0500

    Mostrar horas de los vuelos por envío

--- tasf-frontend/src/features/simulation/components/FlightListModal.tsx
+++ tasf-frontend/src/features/simulation/components/FlightListModal.tsx
@@ -16,14 +16,14 @@ export function FlightListModal({ shipment, onClose }: FlightListModalProps) {
             const parts = flightId.split("-");
             const origin = parts[0] || "???";
             const destination = parts[1] || "???";
-            // const departureTime = parts[2] || "0000"; Descomentar cuando se arreglen las ids
-            // const formattedDeparture = `${departureTime.slice(0, 2)}:${departureTime.slice(2)}`;
+             const departureTime = parts[2] || "0000";
+             const formattedDeparture = `${departureTime.slice(0, 2)}:${departureTime.slice(2)}`;
 
             return (
               <div key={flightId} className="flight-item">
                 <div>{origin} → {destination}</div>
                 <div>ID: {flightId}</div>
-                {/* <div>Salida: {formattedDeparture}</div> */}
+                <div>Salida: {formattedDeparture}</div>
               </div>
             );
           })}
